ba02964ca1dc97157c1abf6f7fe8933d91b023ea
bryxnt1 <bryantperez56@gmail.com>
2023-05-21 20:33:10 -0400

    minor changes

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,7 @@
 import Proj1 from "../assets/proj1.jpg";
-import Proj2 from "../assets/proj2.jpg";
+import Proj2 from "../assets/proj2.png";
 import Proj3 from "../assets/proj3.webp";
-import Proj4 from "../assets/proj4.webp";
+import Proj4 from "../assets/proj4.png";
 import Proj5 from "../assets/proj5.jpg";
 import Proj6 from "../assets/proj6.png";
 
@@ -14,7 +14,7 @@ export const ProjectList = [
   {
     name: "Social Media Website",
     image: Proj4,
-    skills: "React,Node.js,MySQL,GraphQL",
+    skills: "React,Node.js,MySQL,Knox",
   },
   {
     name: "Dashboard Visualizer",
