51cb8ef45046279814971fec72ee3056afe04948
bryxnt1 <bryantperez56@gmail.com>
2023-05-22 21:01:15 -0400

    remove

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,8 +1,5 @@
-import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.png";
-import Proj3 from "../assets/proj3.webp";
 import Proj4 from "../assets/proj4.png";
-import Proj5 from "../assets/proj5.jpg";
 import Proj6 from "../assets/proj6.png";
 
 export const ProjectList = [
